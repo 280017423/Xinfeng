a9953631e908daa6f67a6f82354c86c2db5b4b5b
zou.sq <zou.sq@weare-team.com>
2015-04-24 17:01:22 +0800

    修改UI

--- src/com/zjhbkj/xinfen/fragment/HomeFragment.java
+++ src/com/zjhbkj/xinfen/fragment/HomeFragment.java
@@ -4,9 +4,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +17,8 @@ import com.zjhbkj.xinfen.db.DBMgr;
 import com.zjhbkj.xinfen.model.RcvComsModel;
 import com.zjhbkj.xinfen.udp.UDPServer;
 import com.zjhbkj.xinfen.util.CommandUtil;
+import com.zjhbkj.xinfen.util.EvtLog;
+import com.zjhbkj.xinfen.util.UIUtil;
 
 import de.greenrobot.event.EventBus;
 
@@ -33,6 +37,9 @@ public class HomeFragment extends FragmentBase {
 	private TextView mTvOutInTemp;
 	private TextView mTvOutOutTemp;
 	private TextView mTvHumidity;
+	private View mViewHome;
+	private TextView mTvInM3;
+	private TextView mTvOutM3;
 
 	public static final HomeFragment newInstance() {
 		HomeFragment fragment = new HomeFragment();
@@ -63,6 +70,21 @@ public class HomeFragment extends FragmentBase {
 	private void initViews(View layout) {
 		TextView tvTitle = (TextView) layout.findViewById(R.id.title_with_back_title_btn_mid);
 		tvTitle.setText(R.string.title_home);
+
+		mViewHome = layout.findViewById(R.id.rl_home_tab_layout);
+		int height = UIUtil.getScreenWidth(getActivity()) * 16 / 25;
+		int width = UIUtil.getScreenWidth(getActivity()) * 4 / 5;
+		UIUtil.setViewWidth(mViewHome, width);
+		UIUtil.setViewHeight(mViewHome, height);
+		EvtLog.d("aaa", "屏幕宽度：" + UIUtil.getScreenWidth(getActivity()));
+		EvtLog.d("aaa", "mViewHome宽度：" + UIUtil.getScreenWidth(getActivity()) * 4 / 5);
+		EvtLog.d("aaa", "mViewHome高度：" + height);
+
+		mTvInM3 = (TextView) layout.findViewById(R.id.tv_pm_2dot5_in_title);
+		mTvInM3.setText(Html.fromHtml("mg/m&sup3;"));
+		mTvOutM3 = (TextView) layout.findViewById(R.id.tv_pm_2dot5_out_title);
+		mTvOutM3.setText(Html.fromHtml("mg/m&sup3;"));
+
 		mTvFrequency = (TextView) layout.findViewById(R.id.tv_frequency);
 		mTvPpm = (TextView) layout.findViewById(R.id.tv_ppm);
 		mTvMode = (TextView) layout.findViewById(R.id.tv_mode);
@@ -70,11 +92,19 @@ public class HomeFragment extends FragmentBase {
 		mTvPm2dot5Out = (TextView) layout.findViewById(R.id.tv_pm_2dot5_out_low);
 		mTvPm2dot5In = (TextView) layout.findViewById(R.id.tv_pm_2dot5_in_low);
 		mTvCo2 = (TextView) layout.findViewById(R.id.tv_co2_low);
+
 		mTvInInTemp = (TextView) layout.findViewById(R.id.tv_in_in_wind_temp);
 		mTvInOutTemp = (TextView) layout.findViewById(R.id.tv_in_out_wind_temp);
 		mTvOutInTemp = (TextView) layout.findViewById(R.id.tv_out_in_wind_temp);
 		mTvOutOutTemp = (TextView) layout.findViewById(R.id.tv_out_out_wind_temp);
+
 		mTvHumidity = (TextView) layout.findViewById(R.id.tv_humidity);
+
+		mTvHumidity.setPadding(0, height / 4, 0, 0);
+		mTvOutInTemp.setPadding(width * 1 / 20, height / 4, 0, 0);
+		mTvOutOutTemp.setPadding(0, height / 4, width * 1 / 20, 0);
+		mTvInInTemp.setPadding(width * 3 / 20, height * 9 / 20, 0, 0);
+		mTvInOutTemp.setPadding(0, height * 9 / 20, width * 3 / 20, 0);
 	}
 
 	@Override
@@ -88,32 +118,32 @@ public class HomeFragment extends FragmentBase {
 		if (null == model) {
 			return;
 		}
-		mTvFrequency.setText("" + CommandUtil.hexStringToInt(model.getCommand1()));
-		mTvPpm.setText("" + CommandUtil.hexStringToInt(model.getCommand2()) / 100.0);
+		mTvFrequency.setText("频率：" + CommandUtil.hexStringToInt(model.getCommand1()) + " hz");
+		mTvPpm.setText("甲醛：" + CommandUtil.hexStringToInt(model.getCommand2()) / 100.0 + " ppm");
 		int mode = CommandUtil.hexStringToInt(model.getCommand3());
 		switch (mode) {
 			case 1:
-				mTvMode.setText("自动");
+				mTvMode.setText("自动模式");
 				break;
 			case 2:
-				mTvMode.setText("手动");
+				mTvMode.setText("手动模式");
 				break;
 			case 3:
-				mTvMode.setText("睡眠");
+				mTvMode.setText("睡眠模式");
 				break;
 			default:
 				break;
 		}
-		mTvModeSwitch.setText(1 == CommandUtil.hexStringToInt(model.getCommand4()) ? "开" : "关");
+		mTvModeSwitch.setText(1 == CommandUtil.hexStringToInt(model.getCommand4()) ? "模式开关：开" : "模式开关：关");
 		mTvPm2dot5Out.setText(model.getDisplayPm2dotOut());
 		mTvPm2dot5In.setText(model.getDisplayPm2dotIn());
-		mTvCo2.setText(model.getDisplayCo2());
-		mTvInInTemp.setText("" + CommandUtil.hexStringToInt(model.getCommand11()));
-		mTvInOutTemp.setText("" + CommandUtil.hexStringToInt(model.getCommand12()));
-		mTvOutInTemp.setText("" + CommandUtil.hexStringToInt(model.getCommand13()));
-		mTvOutOutTemp.setText("" + CommandUtil.hexStringToInt(model.getCommand14()));
+		mTvCo2.setText(Html.fromHtml("CO₂") + "：" + model.getDisplayCo2() + " ppm");
+		mTvInInTemp.setText("" + CommandUtil.hexStringToInt(model.getCommand11()) + Html.fromHtml("&#8451;"));
+		mTvInOutTemp.setText("" + CommandUtil.hexStringToInt(model.getCommand12()) + Html.fromHtml("&#8451;"));
+		mTvOutInTemp.setText("" + CommandUtil.hexStringToInt(model.getCommand13()) + Html.fromHtml("&#8451;"));
+		mTvOutOutTemp.setText("" + CommandUtil.hexStringToInt(model.getCommand14()) + Html.fromHtml("&#8451;"));
 
-		mTvHumidity.setText("" + CommandUtil.hexStringToInt(model.getCommand15()));
+		mTvHumidity.setText("湿度：" + CommandUtil.hexStringToInt(model.getCommand15()));
 	}
 
 	public void onEventMainThread(String info) {
